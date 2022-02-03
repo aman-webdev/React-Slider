02e7a595f148bf6b6bba89ba15860315ca6a1ac5
Aman <aman.nabi02@gmail.com>
2022-02-04 00:26:11 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useRef, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { motion } from "framer-motion";
+import images from "./images";
 
 function App() {
-  return <div className="App"></div>;
+  const [width, setWidth] = useState(0);
+  const ref = useRef();
+
+  useEffect(() => {
+    setWidth(ref.current.scrollWidth - ref.current.offsetWidth);
+  }, []);
+
+  return (
+    <div className="App">
+      <motion.div ref={ref} className="carousel">
+        <motion.div
+          drag="x"
+          // whileTap={{ cursor: "grabbing" }}
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
+          {images.map((img, i) => (
+            <motion.div key={i} className="item">
+              <img src={img} alt="" />
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 }
 
 export default App;
